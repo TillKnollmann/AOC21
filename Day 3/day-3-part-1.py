f49b5da25c777b93308fe9ed0b5aa92dae3965bb
Till Knollmann <tillk@mail.upb.de>
2021-12-03 09:38:17 +0100

    - added solution for day 3

--- Day 3/day-3-part-1.py
+++ Day 3/day-3-part-1.py
@@ -1,7 +1,29 @@
-with open("Day x/input.txt", "r") as file:
+import numpy as np
+
+with open("Day 3/input.txt", "r") as file:
     lines = file.readlines()
-    
+    values = []
     for line in lines:
-        break
-    
-    print(" ")
+        for char in line:
+            if not char == "\n":
+                values.append(int(char))
+
+    values = np.array(values).reshape(len(lines), -1)
+    gamma = ""
+    epsilon = ""
+
+    for i in range(0, len(values[0])):
+        if sum(values[:,i]) > (len(values) / 2.):
+            # 1 is most common
+            gamma += "1"
+            epsilon += "0"
+        else:
+            # 0 is most common
+            gamma += "0"
+            epsilon += "1"
+        
+    print("Gamma: " + gamma + " Epsilon: " + epsilon+ "\n")
+    print("Multiplication: " + str(int(gamma,2) * int(epsilon,2)))
+        
+
+
--- Day 3/day-3-part-2.py
+++ Day 3/day-3-part-2.py
@@ -1,7 +1,48 @@
-with open("Day x/input.txt", "r") as file:
+import numpy as np
+
+def find_most_common(input_array, position):
+    if sum(input_array[:, position]) >= len(input_array) / 2.:
+        return 1
+    else:
+        return 0
+
+with open("Day 3/input.txt", "r") as file:
     lines = file.readlines()
-    
+    values = []
     for line in lines:
-        break
-    
-    print(" ")
+        for char in line:
+            if not char == "\n":
+                values.append(int(char))
+
+    values = np.array(values).reshape(len(lines), -1)
+    oxygen_gen_rat = values.copy()
+    co2_scr_rat = values.copy()
+
+    i = 0
+    while i < len(values[0]) and len(oxygen_gen_rat) > 1:
+        most_common = find_most_common(oxygen_gen_rat, i)
+        if len(oxygen_gen_rat) > 1:
+            oxygen_gen_rat = oxygen_gen_rat[oxygen_gen_rat[:,i] == most_common]
+        i += 1
+
+    i = 0
+    while i < len(values[0]) and len(co2_scr_rat) > 1:
+        most_common = find_most_common(co2_scr_rat, i)
+        if len(co2_scr_rat) > 1:
+            co2_scr_rat = co2_scr_rat[co2_scr_rat[:,i] != most_common]
+        i += 1
+
+    chars_to_replace = {
+        '[': '',
+        ']': '',
+        ' ': ''
+    }
+
+    oxy_str = str(oxygen_gen_rat[0]).translate(str.maketrans(chars_to_replace))
+    co2_str = str(co2_scr_rat[0]).translate(str.maketrans(chars_to_replace))
+        
+    print("Oxy: " + oxy_str + " CO2: " + co2_str + "\n")
+    print("Multiplication: " + str(int(oxy_str,2) * int(str(co2_str),2)))
+        
+
+
