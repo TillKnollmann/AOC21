ecd4e00955123ad972af11d5c2f0fb661a4a2a17
Till Knollmann <tillk@mail.upb.de>
2021-12-06 10:09:15 +0100

    - first part solution

--- Day 6/day-6.py
+++ Day 6/day-6.py
@@ -1,17 +1,31 @@
 import numpy as np
 import time
+import bisect
 
-path = "Day 6/input-test.txt"
+path = "Day 6/input.txt"
 
 def main():
 	with open(path, "r") as file:
 		startTime = time.time()
 		lines = file.readlines()
 
-		for line in lines:
-			break
+		total_steps = 256
 
-		print(" ")
+		fish = np.array(lines[0].split(sep=","), np.int32)
+		print("Initial state: " + str(fish))
+
+		for i in range(1,total_steps + 1):
+			zeros = len(fish[fish == 0])
+			# reduce all fish counters
+			fish = fish - 1
+			# set zeros to 6
+			fish[fish==-1] = 6
+			# append eights
+			eights = np.full(zeros, 8)		
+			fish = np.concatenate((fish, eights))
+			print("\nDay " + str(i) + " " + str(fish))
+
+		print("\nThere are " + str(len(fish)) + " fish")
 		executionTime = round(time.time() - startTime, 2)
 		print("Execution time in seconds: " + str(executionTime))
 
