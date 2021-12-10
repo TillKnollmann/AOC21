1c73017bb1ff0055fb39692434fb7a138b57d054
Till Knollmann <tillk@mail.upb.de>
2021-12-10 09:14:21 +0100

    - converted tab stops to whitespace

--- Day 11/day-11.py
+++ Day 11/day-11.py
@@ -3,17 +3,19 @@ import time
 
 path = "Day 11/input-test.txt"
 
+
 def main():
-	with open(path, "r") as file:
-		startTime = time.time()
-		lines = file.readlines()
+    with open(path, "r") as file:
+        startTime = time.time()
+        lines = file.readlines()
 
-		for line in lines:
-			break
+        for line in lines:
+            break
 
-		print(" ")
-		executionTime = round(time.time() - startTime, 2)
-		print("Execution time in seconds: " + str(executionTime))
+        print(" ")
+        executionTime = round(time.time() - startTime, 2)
+        print("Execution time in seconds: " + str(executionTime))
+
 
 if __name__ == "__main__":
-	main()
+    main()
