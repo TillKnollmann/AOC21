9eb083a7c6c4586f34967aee2f6ab1ce545d66dc
Till Knollmann <till-knollmann@outlook.de>
2021-12-12 22:25:23 +0100

    -

--- Day 12/day-12-try.py
+++ Day 12/day-12-try.py
@@ -10,13 +10,44 @@ simple_paths = []
 visited = {}
 
 
+def dfs_iterative(adj_list, start, target, visitedTwice):
+    stack, path = [start], []
+
+    while stack:
+        vertex = stack.pop()
+        if visited[vertex] and not visitedTwice is None:
+            continue
+        if not str.isupper(vertex):
+            if visited[vertex]:
+                if not (vertex == "start" or vertex == "end"):
+                    visitedTwice = vertex
+                else:
+                    continue
+            visited[vertex] = True
+        path.append(vertex)
+        if vertex == target:
+            simple_paths.append(path)
+            visited[vertex] = False
+            continue
+        for neighbor in adj_list[vertex]:
+            stack.append(neighbor)
+        if visitedTwice == vertex:
+            visitedTwice = None
+        else:
+            visited[vertex] = False
+
+    return path
+
+
 def dfs(adj_list, start, target, visitedTwice):
+    print(str(visitedTwice) + " " + str(current_path))
+    param = visitedTwice
     if visited[start] and visitedTwice:
         return
     if not str.isupper(start):
         if visited[start]:
             if not (start == "start" or start == "end"):
-                visitedTwice = True
+                param = True
             else:
                 return
         visited[start] = True
@@ -27,7 +58,7 @@ def dfs(adj_list, start, target, visitedTwice):
         current_path.pop()
         return
     for neighbor in adj_list[start]:
-        dfs(adj_list, neighbor, target, visitedTwice)
+        dfs(adj_list, neighbor, target, param)
     current_path.pop()
     visited[start] = False
 
@@ -35,7 +66,7 @@ def dfs(adj_list, start, target, visitedTwice):
 def main():
     with open(file_path, "r") as file:
 
-        sys.setrecursionlimit(sys.getrecursionlimit() * 4)
+        sys.setrecursionlimit(40)
         startTime = time.time()
         lines = file.readlines()
 
@@ -59,7 +90,7 @@ def main():
             adj_list[edge[0]].append(edge[1])
             adj_list[edge[1]].append(edge[0])
 
-        pprint.pprint(adj_list)
+        # pprint.pprint(adj_list)
 
         # for part 1
 
@@ -67,6 +98,7 @@ def main():
             visited[node] = False
         # dfsOld(adj_list, "start", "end")
         dfs(adj_list, "start", "end", False)
+        # dfs_iterative(adj_list, "start", "target", "dsayf")
         print(str(len(simple_paths)))
 
         for node in nodes:
