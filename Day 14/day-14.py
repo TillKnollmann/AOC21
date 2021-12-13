ab70e8779f1315cdb2bc6bb02cec73aecb3c4320
Till Knollmann <till-knollmann@outlook.de>
2021-12-13 19:23:16 +0100

    - added template using advent-of-code-data

--- Day 14/day-14.py
+++ Day 14/day-14.py
@@ -1,20 +1,28 @@
 import numpy as np
 import time
 import pprint
+from aocd import get_data
+from aocd import submit
 
-path = "Day 14/input-test.txt"
 
 def main():
-    with open(path, "r") as file:
-        startTime = time.time()
-        lines = file.readlines()
 
-        for line in lines:
-            break
+    data = get_data(day=14, year=2021).splitlines()
 
-        print(" ")
-        executionTime = round(time.time() - startTime, 2)
-        print("Execution time in seconds: " + str(executionTime))
+    result_1 = 0
+    result_2 = 0
+
+    print(data)
+    startTime = time.time()
+
+    print(" ")
+    executionTime = round(time.time() - startTime, 2)
+    print("Execution time in seconds: " + str(executionTime))
+
+    # submit(result_1, part="a", day=14, year=2021)
+    # submit(result_2, part="b", day=14, year=2021)
+
 
 if __name__ == "__main__":
-	main()
+    main()
+
