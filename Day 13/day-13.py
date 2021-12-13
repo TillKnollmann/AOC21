fb3a380718e088807c6842727f889df64fdaf9e7
Till Knollmann <tillk@mail.upb.de>
2021-12-13 09:39:07 +0100

    - added solution for day 13 part 2

--- Day 13/day-13.py
+++ Day 13/day-13.py
@@ -9,6 +9,7 @@ def printMatrix(matrix):
     output = matrix.copy().astype("str")
     output[output == "0.0"] = " "
     output[output == "1.0"] = "#"
+    output[output == "-1.0"] = "O"
 
     print("")
     for i in range(0, len(output)):
@@ -26,6 +27,7 @@ def main():
         points = []
         instructions = []
 
+        # parse input
         isInput = True
         for line in lines:
             cleaned = line.replace("\n", "")
@@ -42,40 +44,50 @@ def main():
 
         points = np.array(points)
 
+        # build initial matrix
         matrix = np.zeros((max(points[:, 1]) + 1, max(points[:, 0]) + 1))
 
         for point in points:
             matrix[point[1], point[0]] = 1
 
-        # printMatrix(matrix)
-
         first_fold = None
 
         for instruction in instructions:
-            print(instruction)
-            # print(matrix.shape)
+
             if instruction[0] == "y":
                 # rotate the matrix leftwise
-                matrix = np.rot90(matrix)
+                matrix = np.rot90(matrix, 1).copy()
+
             # split matrix
-            left_part = matrix[:, 0 : int(instruction[1])]
-            right_part = np.fliplr(matrix[:, (len(matrix[0]) - int(instruction[1])) :])
-            # print(left_part.shape)
-            # print(right_part.shape)
+            left_part = matrix[:, 0 : int(instruction[1])].copy()
+
+            # fill right part with trailing zeros
+            right_part = matrix[:, int(instruction[1]) + 1 :].copy()
+            temp = np.zeros(left_part.shape)
+            temp[:, 0 : len(right_part[0])] = right_part
+            right_part = temp.copy()
+
+            # flip the right part
+            right_part = np.fliplr(right_part)
+
+            # fold
             matrix = left_part + right_part
-            # print(matrix)
+            # rotate back
             if instruction[0] == "y":
-                # rotate back
-                matrix = np.rot90(matrix, 3)
-            matrix[matrix > 1] = 1
+                matrix = np.rot90(matrix, 3).copy()
+
+            # normalize
+            matrix[matrix >= 1] = 1
+
+            # determine result part 1
             if first_fold is None:
                 first_fold = sum(matrix[matrix > 0])
-                print(first_fold)
-            # printMatrix(matrix)
 
+        # print result
         printMatrix(matrix)
 
         print("\nResult Part 1: " + str(first_fold))
+
         executionTime = round(time.time() - startTime, 2)
         print("Execution time in seconds: " + str(executionTime))
 
