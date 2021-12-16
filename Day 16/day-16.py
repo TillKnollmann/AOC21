d9d39d62a817bf258591229b3fa8698b53a35435
Till Knollmann <tillk@mail.upb.de>
2021-12-16 08:23:08 +0100

    - first try

--- Day 16/day-16.py
+++ Day 16/day-16.py
@@ -2,6 +2,7 @@ from datetime import date
 import numpy as np
 import time
 import pprint
+import math
 
 from importlib.machinery import SourceFileLoader
 
@@ -15,11 +16,108 @@ path = ""
 
 
 def parseInput(input):
-    result = None
+    return str((bin(int(input[0], base=16))[2:]).zfill(len(input[0]) * 4))
 
-    # Todo Input parsing
 
-    return result
+def parseBinString(bin_string):
+    try:
+        orig = str(bin_string)
+        packet_version = int(bin_string[0:3], 2)
+        packet_typeID = int(bin_string[3:6], 2)
+        bin_string = bin_string[6:]
+        print("".join(["\n", orig, " ", str(packet_version), " ", str(packet_typeID)]))
+
+        if packet_typeID == 4:
+            # literal
+            groups = [
+                (bin_string[i * 5 : i * 5 + 1], bin_string[i * 5 + 1 : i * 5 + 5])
+                for i in range(math.floor(len(bin_string) / 5.0))
+            ]
+
+            groups_new = []
+            for i in range(len(groups)):
+                groups_new.append(groups[i][1])
+                if groups[i][0] == "0":
+                    break
+            groups = groups_new
+
+            # cut binstring
+            bin_string = (
+                bin_string[len(groups) * 5 :]
+                if len(bin_string) > len(groups) * 5 and int(bin_string, 2) != 0
+                else ""
+            )
+
+            number = int("".join(groups), 2) if len(groups) > 0 else 0
+
+            print(number)
+
+            if len(bin_string) > 0 and not int(bin_string, 2) == 0:
+                return (
+                    (packet_version, packet_typeID, number),
+                    parseBinString(bin_string),
+                )
+            else:
+                return (packet_version, packet_typeID, number)
+
+        else:
+            length_type_ID = bin_string[0]
+            bin_string = bin_string[1:]
+            print("".join(["Length Type ID: ", str(length_type_ID)]))
+
+            if length_type_ID == "0":
+                # is 0
+                length = int(bin_string[0:15], 2)
+                print("Length " + str(length))
+                bin_string = bin_string[15:]
+                sub_packets = parseBinString(bin_string[0:length])
+                bin_string = bin_string[length:]
+
+                if len(bin_string) > 0 and not int(bin_string, 2) == 0:
+                    return (
+                        (packet_version, packet_typeID, sub_packets),
+                        parseBinString(bin_string),
+                    )
+                else:
+                    return (packet_version, packet_typeID, sub_packets)
+            if length_type_ID == "1":
+                length = int(bin_string[0:15], 2)
+                print("Length " + str(length))
+                bin_string = bin_string[15:]
+                if length == 0:
+                    return (packet_version, packet_typeID, ())
+                else:
+                    potential_subpackets = parseBinString(bin_string)
+                    sub_packets = (
+                        potential_subpackets[0:length]
+                        if len(potential_subpackets) > length
+                        else potential_subpackets
+                    )
+                    if len(potential_subpackets) > length:
+                        return (
+                            (packet_version, packet_typeID, sub_packets),
+                            potential_subpackets[length:],
+                        )
+                    else:
+                        return (packet_version, packet_typeID, sub_packets)
+    except Exception as e:
+        print("Parsing Error!")
+        return
+
+
+def getVersionSum(result):
+    try:
+        iter(result)
+        if len(result) == 3:
+            # valid packet
+            return result[0] + getVersionSum(result[2])
+        elif len(result) > 1:
+            # a list of packets
+            return sum(map(getVersionSum, result))
+        else:
+            return 0
+    except TypeError:
+        return 0
 
 
 def part1(data, measure=False):
@@ -27,6 +125,11 @@ def part1(data, measure=False):
     result_1 = None
 
     input = parseInput(data)
+
+    result = parseBinString(input)
+    print(result)
+
+    result_1 = getVersionSum(result)
 
     # Todo program part 1
 
@@ -87,7 +190,7 @@ def main():
     global path
     path = "Day " + str(day) + "/"
 
-    test_sol = []  # Todo put in test solutions
+    test_sol = [6, 9, 14, 16, 12, 23, 31]  # Todo put in test solutions
 
     test = True  # Todo
 
