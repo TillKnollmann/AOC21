a4b0a34a628cb9293777db950660164ed8a0dce1
Till Knollmann <tillk@mail.upb.de>
2021-12-17 09:50:03 +0100

    - added solution for day 17

--- Day 17/day-17.py
+++ Day 17/day-17.py
@@ -2,6 +2,7 @@ from datetime import date
 import numpy as np
 import time
 import pprint
+import math
 
 from importlib.machinery import SourceFileLoader
 
@@ -15,11 +16,249 @@ path = ""
 
 
 def parseInput(input):
-    result = None
+    """Parses the input for today's puzzle
 
-    # Todo Input parsing
+    Args:
+        input (list<str>): input array of length 1
 
-    return result
+    Returns:
+        tuple<tuple,tuple>: valid area. First part is x dimension, second part y dimension
+    """
+
+    split = input[0].replace("\n", "").split(" ")
+
+    t_area_x = (
+        int(split[2].split("..")[0][2:]),
+        int(split[2].split("..")[1][:-1]),
+    )
+    t_area_y = (int(split[3].split("..")[0][2:]), int(split[3].split("..")[1]))
+
+    return (t_area_x, t_area_y)
+
+
+def generatePosition(pos, vel):
+    """Generates new positions based on a position vector and a veloctiy
+
+    Args:
+        pos (tuple<int,int>): current positions in x,y
+        vel (tuple<int,int>): velocity vector
+
+    Returns:
+        tuple<tuple<int,int>,tuple<int,int>: new position and new velocity
+    """
+    pos_x, pos_y = pos
+    vel_x, vel_y = vel
+    new_pos = (pos_x + vel_x, pos_y + vel_y)
+    new_vel = (vel_x - np.sign(vel_x), vel_y - 1)
+    return (new_pos, new_vel)
+
+
+def isInTarget(pos, area):
+    """Returns True if pos is in area
+
+    Args:
+        pos (tuple): The current position
+        area (tuple<tuple,tuple>): The bounds of the tarbet area
+
+    Returns:
+        bool: True if pos is in area
+    """
+    if area[0][0] <= pos[0] <= area[0][1] and area[1][0] <= pos[1] <= area[1][1]:
+        return True
+    return False
+
+
+def isBeforeTarget(pos, area):
+    """Returns False if the position cannot meet the area no matter how the velocity is
+
+    Args:
+        pos (tuple): current position
+        area (tuple<tuple,tuple>): target area
+
+    Returns:
+        bool: False if it is impossible to meet the area from this position
+    """
+    if pos[0] > area[0][1]:
+        # The x value is already too large
+        return False
+    elif pos[1] < area[1][0]:
+        # The y value is already too small
+        return False
+    else:
+        return True
+
+
+def getHeighest(init_pos, init_vel, t_area, Print=False):
+    """Simulates movement based on initial position and velocity and returns if the area is met and what the maximum height is
+
+    Args:
+        init_pos (tuple): initial position
+        init_vel (tuple): initial velocity
+        t_area (tuple<tuple>): target area
+        Print (bool, optional): Whether to print logs. Defaults to False.
+
+    Returns:
+        tuple: value 1 is the maximum height, value 2 is whether t_area is met
+    """
+
+    highest_y = 0
+
+    pos = init_pos
+    vel = init_vel
+
+    reaches = False
+
+    while isBeforeTarget(pos, t_area):
+        # get new position
+        pos, vel = generatePosition(pos, vel)
+        # update height
+        highest_y = max(highest_y, pos[1])
+        if isInTarget(pos, t_area):
+            # set flag
+            reaches = True
+            if Print:
+                print(str(init_vel) + " reaches target with height " + str(highest_y))
+
+    if not reaches:
+        if Print:
+            print(str(init_vel) + " misses target with height " + str(highest_y))
+
+    return highest_y, reaches
+
+
+def gaus(n):
+    """Returns the result of the gaussian sum
+
+    Args:
+        n (int): parameter
+
+    Returns:
+        int: gaussian sum of n
+    """
+    return (math.pow(n, 2) + n) / 2.0
+
+
+def isXinArea(x, area):
+    """Checks if x is in its bounds based on area
+
+    Args:
+        x (int): x coordinate
+        area (tuple): target area
+
+    Returns:
+        bool: True if x is within the range given by area
+    """
+    # print("Check if " + str(x) + " is in " + str(area[0]))
+    if area[0][0] <= x <= area[0][1]:
+        return True
+    return False
+
+
+def isYinArea(y, area):
+    """Checks if y is in its bounds based on area
+
+    Args:
+        y (int): y coordinate
+        area (tuple): target area
+
+    Returns:
+        bool: True if y is within the range given by area
+    """
+    if area[1][0] <= y <= area[1][1]:
+        return True
+    return False
+
+
+def initialXreachesArea(x, area):
+    """Returns true if an initial velocity of x in x direction could reach the target area
+
+    Args:
+        x (int): initial x velocity
+        area (tuple): target area
+
+    Returns:
+        bool: True if the initial x velocity could reach the target area
+    """
+    # x behaves according to a gaussian distribution
+    # i represents the step that we consider
+    temp = [gaus(x) - gaus(i) for i in range(x)]
+    temp = [isXinArea(temp_value, area) for temp_value in temp]
+    return int(sum(temp) > 0)
+
+
+def initialYreachesArea(y, area):
+    """Returns true if an initial y velocity could reach the target area
+
+    Args:
+        y (int): y velocity
+        area (tuple): target area
+
+    Returns:
+        bool: True if the initial y velocity could reach the target area
+    """
+    if y > 0:
+        # if y is positive, we reach a maximum height of gaus(y)
+        # after that, the distance to the target area is at most gaus(y) - area[1][0]
+        # (since area is negative)
+        # each step i after the maximum height behaves according to gaus(i)
+        temp = [gaus(y) - gaus(i) for i in range(-area[1][0] + y)]
+        temp = [isYinArea(temp_value, area) for temp_value in temp]
+        return int(sum(temp) > 0)
+    else:
+        # if y is negative, i steps substract gaus(i) from the position with an offset of y
+        temp = [gaus(i) + y for i in range(-area[1][0])]
+        temp = [isYinArea(-temp_value, area) for temp_value in temp]
+        return int(sum(temp) > 0)
+
+
+def getPossibleVelocities(area):
+    """Returns possible velocities that could reach the area, but significantly less than all possible velocities
+
+    Args:
+        area (tuple): target area
+
+    Returns:
+        list: Velocities that may reach the area
+    """
+    # get possible x values:
+    x_vels = []
+    for x in range(area[0][1] + 1):
+        if initialXreachesArea(x, area):
+            x_vels.append(x)
+
+    # get possible y values:
+    y_vels = []
+    for y in range(area[1][0] - 1, -area[1][0] + 1):
+        if initialYreachesArea(y, area):
+            y_vels.append(y)
+
+    # generate possible starting velocities
+    possibilities = [(x, y) for x in x_vels for y in y_vels]
+
+    return possibilities
+
+
+def getValidVelocities(velocities, area):
+    """Returns all velocities that reach the target area and the maximum valid height
+
+    Args:
+        velocities (list): list of initial velocities
+        area (tuple): target area
+
+    Returns:
+        list, int: list of velocities that reach the target area, maximum valid height
+    """
+    results = []
+
+    # evaluate
+    number = 0
+    for value in velocities:
+        res = getHeighest((0, 0), value, area)
+        if res[1]:
+            number += 1
+            results.append(res[0])
+
+    return results, number
 
 
 def part1(data, measure=False):
@@ -28,7 +267,11 @@ def part1(data, measure=False):
 
     input = parseInput(data)
 
-    # Todo program part 1
+    area = input
+
+    results, number = getValidVelocities(getPossibleVelocities(area), area)
+
+    result_1 = max(results)
 
     executionTime = round(time.time() - startTime, 2)
     if measure:
@@ -38,11 +281,15 @@ def part1(data, measure=False):
 
 def part2(data, measure=False):
     startTime = time.time()
-    result_2 = None
+    result_2 = 0
 
     input = parseInput(data)
 
-    # Todo program part 2
+    area = input
+
+    results, number = getValidVelocities(getPossibleVelocities(area), area)
+
+    result_2 = number
 
     executionTime = round(time.time() - startTime, 2)
     if measure:
@@ -96,12 +343,12 @@ def main():
     global path
     path = "Day " + str(day) + "/"
 
-    test_sol = []  # Todo put in test solutions
+    test_sol = [45, 112]  # Todo put in test solutions
 
     test = True  # Todo
 
-    sol1 = sub1 = False  # Todo
-    sol2 = sub2 = False  # Todo
+    sol1 = sub1 = True  # Todo
+    sol2 = sub2 = True  # Todo
 
     if test:
         if not runTests(test_sol, path):
